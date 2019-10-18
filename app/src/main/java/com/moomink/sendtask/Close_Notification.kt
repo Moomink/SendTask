fd6dcca605f7eae1e2449cfcbf6da800231d88aa
Moomink <kasutamu907@gmail.com>
2019-10-18 23:50:28 +0900

    IntentとPendingintentの宣言

--- app/src/main/java/com/moomink/sendtask/Close_Notification.kt
+++ app/src/main/java/com/moomink/sendtask/Close_Notification.kt
@@ -1,5 +1,11 @@
 package com.moomink.sendtask
 
-class Close_Notification {
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+
+
+class Close_Notification:AppCompatActivity() {
+    val re_Intent:Intent = intent
+    task.cancel() //TODO 10/18 ここから 関数呼び出し
 
 }
--- app/src/main/java/com/moomink/sendtask/MainActivity.kt
+++ app/src/main/java/com/moomink/sendtask/MainActivity.kt
@@ -2,6 +2,8 @@ package com.moomink.sendtask
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
 import android.content.Context
 import android.os.Build
 import android.os.Bundle
@@ -10,7 +12,6 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import kotlinx.android.synthetic.main.activity_main.*
-
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,71 +20,66 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+
+
+
     var cnt = 0
-    var list:MutableList<String> = mutableListOf()
+
+
 
 
     /*getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.createNotificationChannel(channel)*/
-    fun notification(view: View) {
-        val words: String = editText.text.toString()
-        val subTitle: String = SubTitle.text.toString()
-        val title: String = Title.text.toString()
+     fun notification(view: View){
+        val words:String = TEXT.text.toString()
+        val title:String = Title.text.toString()
+        val test:String = SubTitle.text.toString()
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            if(!list.contains(title)){
-                list.add(title)
-            }
-            val id = """${title}_${list.indexOf(title)}"""
+            val name = getString(R.string.channel_name)
+            val id = getString(R.string.channel_ID)
+            val descriptionText = getString(R.string.description)
             val importance = NotificationManager.IMPORTANCE_HIGH
-            val channel = NotificationChannel(id,title,importance)
-//            val id = getString(R.string.channel_ID)
-//            val descriptionText = getString(R.string.description)
-//            val importance = NotificationManager.IMPORTANCE_DEFAULT
-//            val channel = NotificationChannel(id, title, importance)
-//                .apply { description = descriptionText }
-
-//            var intent:Intent = Intent(this,Close_Notification)
-//            val Result:PendingIntent = PendingIntent(this,cnt,)
+            val channel = NotificationChannel(id,name,importance)  //TODO チャンネル整理
+                                                .apply{description = descriptionText}
+            val intent:Intent = Intent(this,Close_Notification::class.java)
+                .putExtra("ID",cnt)
+            val Result: PendingIntent = PendingIntent.getActivity(this,cnt,intent,PendingIntent.FLAG_CANCEL_CURRENT)
 
 
             val task = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             task.createNotificationChannel(channel)
 
-            val base: NotificationCompat.Builder =
-                NotificationCompat.Builder(applicationContext, title)
-                    .setContentText(words)
-                    .setContentInfo(title)
-                    .setContentTitle(subTitle)
-                    .setSmallIcon(R.drawable.icon_notification)
-                    .setOngoing(true)
-                    .setTicker(getString(R.string.Sent))
-//                .addAction(R.drawable.icon_notification,"完了",)
+            val base:NotificationCompat.Builder = NotificationCompat.Builder(applicationContext,id)
+                .setContentTitle(title)
+                .setContentText(words)
+                .setContentInfo(test)
+                .setSmallIcon(R.drawable.icon_notification)
+                .setOngoing(true)
+                .addAction(R.drawable.icon_notification,"完了",Result)
 
 
-            task.notify(list.indexOf(title), base.build())
-        } else {
+            task.notify(cnt,base.build())   //fixme cntをどうにかする
+        }else{
             val task = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-            val base: NotificationCompat.Builder = NotificationCompat.Builder(applicationContext)
-                .setContentText(subTitle)
+            val base:NotificationCompat.Builder = NotificationCompat.Builder(applicationContext)
+                .setContentText(words)
                 .setContentTitle(title)
-                .setContentInfo(title)
-                .setTicker(getString(R.string.Sent))
+                .setSubText(test)
                 .setSmallIcon(R.drawable.icon_notification)
 
 //            var intent = Intent(this,Close_Notification.class)
 
 
-            task.notify(cnt, base.build())
+            task.notify(cnt,base.build())
         }
 
 
-        Log.d("Debug", cnt.toString())
+        Log.d("Debug",cnt.toString())
         cnt++
     }
-
-    fun close_Notification(view: View) {
-        val task = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+     fun close_Notification(view:View){
+         val task = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         task.cancelAll()
     }
 
